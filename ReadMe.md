3f5f0694779387ed1e0b5990c70f46499ca04b6d
tamamadesu <liyujie1@gmail.com>
2021-03-11 10:02:14 +0800

    Add files via upload
    lsp-eslint  test

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+LSP: 1.2.12
+LSP-eslint: 1.4.2
+LSP-typescript: 1.2.3
---
+++ index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IProps {
+    title: string;
+}
+
+const EslintText = (props: IProps) => {
+    return (
+        <div>Eslint Test</div>
+    );
+}
